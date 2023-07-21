adbf1afc6ce3a7f555748b4cb4e66d17efa448cf
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-21 20:05:06 +0500

    Al

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Info {
+
     public static void main(String[] args) {
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
