995330d0ad3f3512f6bab234d101855d0ae2d7d7
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-21 19:55:02 +0500

    Al

--- src/main/java/ru/job4j/StudenInfo.java
+++ src/main/java/ru/job4j/StudenInfo.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class StudenInfo {
+
     public static void main(String[] args) {
         System.out.println("Александр Игоревич");
         System.out.println("25.01.1995");
