2e0a9894bf485b242faa3b2a1b86a4d201a93e73
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-24 15:01:43 +0500

    4. Методы [#231984 # [#231984]]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,9 +1,14 @@
 package ru.job4j.calculator;
 
+import java.text.DecimalFormat;
+
 public class Calculator {
+    static int one;
+    static int two;
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
+        one = 1;
+        two = 2;
         int six = 6;
         int four = 4;
         int five = 5;
@@ -20,5 +25,14 @@ public class Calculator {
         System.out.printf("new one: %s, new two: %s", one, two);
         size = size - 5;
         System.out.println(size);
+        // Отступ
+        plus(123, 2131);
+    }
+
+    static void plus(int a, int b) {
+        DecimalFormat dF = new DecimalFormat("#,###");
+        one = a; two = b;
+        int result = one + two;
+        System.out.println(dF.format(result));
     }
 }
