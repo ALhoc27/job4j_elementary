a0f3d70fb7678ecac4fd8003621f4fa7593e7638
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-24 16:08:35 +0500

    4.1. Аргументы [#295521 # [#295521]]

---
+++ src/main/java/ru/job4j/mytest/ArgMyMethod.java
@@ -0,0 +1,31 @@
+package ru.job4j.mytest;
+
+    public class ArgMyMethod {
+        static String nameMy = "";
+        String str;
+
+        ArgMyMethod() {
+            System.out.println("example");
+        }
+
+        ArgMyMethod(String str) {
+            this();
+            this.str = str;
+        }
+
+        public static void hello() {
+            System.out.println("Hello, " + nameMy);
+        }
+
+        public void helloMy(String nameMy) {
+            System.out.println(nameMy + " экземпляр");
+        }
+
+        public static void main(String[] args) {
+            nameMy = "Petr Arsentev";
+            ArgMyMethod.hello();
+
+            ArgMyMethod helloMyConstr = new ArgMyMethod();
+            helloMyConstr.helloMy("Sasha");
+    }
+}
