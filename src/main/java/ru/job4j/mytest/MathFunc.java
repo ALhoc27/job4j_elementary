475ef2866771a392e36112c51c9bb028f8196d02
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-24 19:36:24 +0500

    4.1. Аргументы [#295521 # [#295521]]

---
+++ src/main/java/ru/job4j/mytest/MathFunc.java
@@ -0,0 +1,27 @@
+package ru.job4j.mytest;
+
+public class MathFunc {
+    public static int func(int x) {
+        int y = x * x + 1;
+        System.out.println(y);
+        return y;
+    }
+
+    public static int func1(int x) {
+        int y = 1 / x;
+        System.out.println(y);
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int rez1 = MathFunc.func(3);
+        int rez2 = MathFunc.func1(2);
+
+        int total = rez1 + rez2;
+        System.out.println(total);
+
+        int result3 = MathFunc.func1(100);
+        System.out.println(result3);
+    }
+
+}
