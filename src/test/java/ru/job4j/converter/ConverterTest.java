6ef6c5a73e16c3cc90fe972f8f3933d5de08c1c2
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-26 06:14:28 +0500

    1. Модульные тесты. [#159435 # [#159435]]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.Assert;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+        System.out.println("whenConvert140RblThen2Euro " + expected + " / " + out);
+    }
+
+    @Test
+    void whenConvert180RblThen3Dlr() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+        System.out.println("whenConvert180RblThen3Dlr " + expected + " / " + out);
+    }
+
+    @Test
+    void whenConvert200RblThen3dot3333Dlr() {
+        float in = 200;
+        float expected = 3.3333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+        System.out.println("whenConvert200RblThen3dot3333Dlr " + expected + " / " + out);
+    }
+
+    @Test
+    void whenConvert140EuroThen9800rub() {
+        float in = 140;
+        float expected = 9800;
+        float out = Converter.eurotoRuble(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+        System.out.println("whenConvert140EuroThen9800rub " + expected + " / " + out);
+    }
+
+    @Test
+    void whenConvert140DlrThen9800rub() {
+        float in = 140;
+        float expected = 8400;
+        float out = Converter.dollartoRuble(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+        System.out.println("whenConvert140DlrThen9800rub " + expected + " / " + out);
+    }
+}
