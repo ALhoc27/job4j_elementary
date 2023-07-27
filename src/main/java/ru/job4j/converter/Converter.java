adecf256171a674641b4b7c2a48a572c4644fe71
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-27 12:40:02 +0500

    Game

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -30,40 +30,40 @@ public class Converter {
         float value = 140f;
 
         float in = value;
-        float expectedeurotoR1 = 9800;
-        float out1 = Converter.eurotoRuble(in);
-        boolean passedsf1 = expectedeurotoR1 == out1;
-        float expectedeurotoR2 = 8400;
-        float out2 = Converter.dollartoRuble(in);
-        boolean passedsf2 = expectedeurotoR2 == out2;
-        float expectedeurotoR3 = 2;
-        float out3 = Converter.rubleToEuro(in);
-        boolean passedsf3 = expectedeurotoR3 == out3;
-        float expectedeurotoR4 = 2.3333333f;
-        float out4 = Converter.rubleToDollar(in);
-        boolean passedsf4 = expectedeurotoR4 == out4;
+        float expectedeurotoER = 9800;
+        float outER = Converter.eurotoRuble(in);
+        boolean passedsf = expectedeurotoER == outER;
+        float expectedeurotoDR = 8400;
+        float outDR = Converter.dollartoRuble(in);
+        boolean passedsfq = expectedeurotoDR == outDR;
+        float expectedeurotoRE = 2;
+        float outRE = Converter.rubleToEuro(in);
+        boolean passedsfw = expectedeurotoRE == outRE;
+        float expectedeurotoRD = 2.3333333f;
+        float outRD = Converter.rubleToDollar(in);
+        boolean passedsfe = expectedeurotoRD == outRD;
 
         System.out.println("Курс долара: " + courseofdolarRub + " rub");
         System.out.println("Курс евро: " + courseofEuroRub + " rub");
         System.out.println("");
 
         float eurotoR = eurotoRuble(value);
-        String sf1 = String.format("%,.2f", eurotoR);
-        System.out.println(value + " euro are " + sf1 + " rub.");
+        String sfEurotoR = String.format("%,.2f", eurotoR);
+        System.out.println(value + " euro are " + sfEurotoR + " rub.");
         float dollartoR = dollartoRuble(value);
-        String sf2 = String.format("%,.2f", dollartoR);
-        System.out.println(value + " dollar are " + sf2 + " rub.");
+        String sfDollartoR = String.format("%,.2f", dollartoR);
+        System.out.println(value + " dollar are " + sfDollartoR + " rub.");
         float rubtoEuro = Converter.rubleToEuro(value);
-        String sf3 = String.format("%,.2f", rubtoEuro);
-        System.out.println(value + " rubles are " + sf3 + " euro.");
+        String sfRubtoEuro = String.format("%,.2f", rubtoEuro);
+        System.out.println(value + " rubles are " + sfRubtoEuro + " euro.");
         float rubtodDollar = rubleToDollar(value);
-        String sf4 = String.format("%,.2f", rubtodDollar);
-        System.out.println(value + " rubles are " + sf4 + " dollar.");
+        String sfRubtodDollar = String.format("%,.2f", rubtodDollar);
+        System.out.println(value + " rubles are " + sfRubtodDollar + " dollar.");
 
         System.out.println("\nTest: ");
-        System.out.println(value + " euro are " + expectedeurotoR1 + "   Test result : " + passedsf1);
-        System.out.println(value + " dollar are " + expectedeurotoR2 + "   Test result : " + passedsf2);
-        System.out.println(value + " rubles are " + expectedeurotoR3 + "   Test result : " + passedsf3);
-        System.out.println(value + " rubles are " + expectedeurotoR4 + "   Test result : " + passedsf4);
+        System.out.println(value + " euro are " + expectedeurotoER + "   Test result : " + passedsf);
+        System.out.println(value + " dollar are " + expectedeurotoDR + "   Test result : " + passedsfq);
+        System.out.println(value + " rubles are " + expectedeurotoRE + "   Test result : " + passedsfw);
+        System.out.println(value + " rubles are " + expectedeurotoRD + "   Test result : " + passedsfe);
     }
 }
