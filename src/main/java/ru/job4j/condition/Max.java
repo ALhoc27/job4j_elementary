96d049902c4dbf93d994d4e2b8135668633a7d7b
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-27 15:18:22 +0500

    4.2. Тернарное сравнение [#189 # [#189]]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,20 @@
 package ru.job4j.condition;
 
+import java.util.Random;
+
 public class Max {
+
     public static int max(int left, int right) {
         int result = left > right ? left : right;
         return result;
+        }
+
+    public static int numEqual(int left, int right) {
+        int equalNumber = 0;
+        if (left == right) {
+            equalNumber = left;
+            return equalNumber;
+        }
+        return equalNumber;
     }
 }
--- src/test/java/ru/job4j/condition/MaxNumberTest.java
+++ src/test/java/ru/job4j/condition/MaxNumberTest.java
@@ -2,15 +2,28 @@ package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.linesOf;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaxNumberTest {
+    int left;
+    int right;
 
     @Test
-    void maxNumTest() {
-        int result = Max.max(5, 7);
+    void whenMax1To2Then2() {
+        left = 5; right = 7;
+        int result = Max.max(left, right);
         int expected = 7;
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1AndMax2Then() {
+        left = 7; right = 7;
+        int result = Max.numEqual(left, right);
+        assertThat(left).isEqualTo(result);
     }
 }
