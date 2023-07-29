a3e2feef36bd793690c5bf56cc986ebf070db07c
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-29 08:24:43 +0500

    5.5. Простое число (оператор прерывания break) [#171694 # [#171694 #432985]]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,10 +2,20 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number % 2 != 0 && number != 1) {
-            return false;
-        } else {
-            return true;
+        int count = 0;
+        boolean status = false;
+        status = number != 1 ? status : status;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
         }
+        if ((count <= 2) && (count >= 2)) {
+            status = true;
+            return true;
+        } else if (status) {
+            return status;
+        }
+        return status;
     }
 }
--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -10,19 +10,40 @@ class CheckPrimeNumberTest {
     void when5() {
         int number = 5;
         boolean result = CheckPrimeNumber.check(number);
-        assertThat(result).isFalse();
+        assertThat(result).isTrue();
     }
 
     @Test
     void when4() {
         int number = 4;
         boolean result = CheckPrimeNumber.check(number);
-        assertThat(result).isTrue();
+        assertThat(result).isFalse();
     }
 
     @Test
     void when1() {
         int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when11() {
+        int number = 11;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when29() {
+        int number = 29;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when23() {
+        int number = 23;
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isTrue();
     }
