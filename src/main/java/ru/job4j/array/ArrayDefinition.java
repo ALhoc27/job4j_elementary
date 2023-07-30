187170b5eae411df82d9ece682babf8437ad8187
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-30 20:38:44 +0500

    6.0.2. Размер массива [#333582 # [#333582]]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,15 +1,24 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] priced = new float[40];
+        String[] names = new String[4];
 
         ArrayDefinition array = new ArrayDefinition();
         array.numberElements(priced);
         array.numberElements(surnames);
         array.numberElements(ages);
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Gav";
+        names[2] = "Naf-naf";
+        names[3] = "Red";
+        System.out.println("names: " + Arrays.stream(names).toList());
     }
 
    public void numberElements(short[] index) {
