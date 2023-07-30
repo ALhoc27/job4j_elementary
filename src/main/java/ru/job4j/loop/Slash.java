ac099068bd1204b37cd098ed518247ba14a2362a
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-30 16:53:42 +0500

    5.9. Крест в псевдографике.  [#177182 # [#177182]]

---
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+public class Slash {
+
+    public static void draw(int size) {
+        System.out.println("Draw by " + size);
+        int tempRow = size - 1;
+        int tempCell = -1;
+        for (int row = 0; row < size; row++) {
+            tempCell++;
+            tempRow--;
+            for (int cell = 0; cell < size; cell++) {
+                if (cell == tempCell && tempCell != size - 1) {
+                    System.out.print("Xc");
+                } else if (tempRow == cell) {
+                    System.out.print("Xr");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        draw(7);
+        draw(9);
+    }
+}
