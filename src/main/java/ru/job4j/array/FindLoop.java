6f5d6d7e2a471bd467fde0ccb0b4ddb50ff1285f
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-07-31 19:35:17 +0500

    6.5. Поиск индекса в диапазоне. [#156318 # [#156318]]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,48 @@
+package ru.job4j.array;
+
+public class FindLoop {
+
+    public static int indexOf(int[] data, int el) {
+        int result = 0;
+
+        for (int i = 0; i <= data.length - 1; i++) {
+            if (data[i] == el) {
+                result = i;
+                break;
+            } else {
+                result = -1;
+            }
+        }
+        if (result == -1) {
+            System.out.println("Элемент не найден!");
+        }
+        return result;
+    }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int result = 0;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+               result = i;
+               break;
+            } else {
+                result = -1;
+            }
+        }
+        if (result == -1) {
+            System.out.println("Элемент не найден!");
+        }
+        return result;
+    }
+
+    public static int indexOfSixOne(int[] data, int el) {
+        int rst = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+}
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -17,10 +17,41 @@ class FindLoopTest {
     }
 
     @Test
+    void whenArrayHasNot10ThenMinus58() {
+        int[] data = new int[] {5, 10, 3, 1, 6, 36, 336, 1, 0, 9, 43};
+        int el = 3;
+        int start = 8;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHasNot10ThenMinus5() {
+        int[] data = new int[] {5, 10, 3, 1, 6, 3};
+        int el = 3;
+        int start = 4;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas5Then3() {
+        int[] data = new int[] {5, 10, 3, 1, 36, 1};
+        int el = 1;
+        int result = FindLoop.indexOfSixOne(data, el);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     void whenArrayHasNot10ThenMinus1() {
         int[] data = new int[] {2, 4, 8, 6, 9};
         int el = 6;
-        int result = FindLoop.indexOf(data, el);
+        int result = FindLoop.indexOfSixOne(data, el);
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
