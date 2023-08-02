69a5224e191bb8817f1fc127743626a9d9e7b176
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-02 21:29:25 +0500

    .

--- src/main/java/ru/job4j/array/LengthArrayArrays.java
+++ src/main/java/ru/job4j/array/LengthArrayArrays.java
@@ -5,9 +5,8 @@ public class LengthArrayArrays {
         int[][] numbers = {{1}, {2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
 
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers[i].length; j++) {
-                System.out.print(numbers[i][j] + " ");
-            }
+                System.out.print("Размер вложенного массива [" + i + "]  = " + numbers[i].length);
+            System.out.println("");
         }
     }
 }
