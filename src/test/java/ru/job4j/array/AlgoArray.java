c33e65e328dc9a180610164a6bcc19cae675e584
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-03 22:47:51 +0500

    .

--- src/test/java/ru/job4j/array/AlgoArray.java
+++ src/test/java/ru/job4j/array/AlgoArray.java
@@ -1,7 +1,7 @@
 package ru.job4j.array;
 
 public class AlgoArray {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         int[] array = new int[] {3, 5, 2, 1, 4};
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
@@ -16,5 +16,22 @@ public class AlgoArray {
         for (int index = 0; index < array.length; index++) {
             System.out.print(array[index] + " ");
         }
+     */
+
+        public static void main(String[] args) {
+            int[] array = new int[] {5, 3, 2, 1, 4};
+            int temp = array[0];
+            array[0] = array[3];
+            array[3] = temp;
+            temp = array[3];
+            array[3] = array[4];
+            array[4] = temp;
+            temp = array[1];
+            array[1] = array[2];
+            array[2] = temp;
+
+            for (int index = 0; index < array.length; index++) {
+                System.out.println(array[index]);
+            }
+        }
     }
-}
