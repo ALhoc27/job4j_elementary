ab91f3da374ba396e8d7bdbeb73cac3f4e1933a2
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-03 22:37:29 +0500

    .

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -1,10 +1,19 @@
 package ru.job4j.array;
 
 public class Turn {
+//    public static int[] back(int[] array) {
+//        int[] temp = new int[array.length];
+//        for (int i = 0; i <= array.length - 1; i++) {
+//            temp[temp.length - 1 - i] = array[i];
+//        }
+//        return temp;
+//    }
+
     public static int[] back(int[] array) {
         int[] temp = new int[array.length];
-        for (int i = 0; i <= array.length - 1; i++) {
+        for (int i = 0; i <= array.length / 2; i++) {
             temp[temp.length - 1 - i] = array[i];
+            temp[i] = array[array.length - 1 - i];
         }
         return temp;
     }
