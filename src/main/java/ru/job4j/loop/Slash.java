7e310b753fc01e5bdadedb3b93a8aef851103313
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-03 14:04:08 +0500

    .

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -4,15 +4,15 @@ public class Slash {
 
     public static void draw(int size) {
         System.out.println("Draw by " + size);
-        int tempRow = size - 1;
-        int tempCell = -1;
+//        int tempRow = size - 1;
+//        int tempCell = -1;
         for (int row = 0; row < size; row++) {
-            tempCell++;
-            tempRow--;
+//            tempCell++;
+//            tempRow--;
             for (int cell = 0; cell < size; cell++) {
-                if (cell == tempCell && tempCell != size - 1) {
+                if (row + cell == size - 1) {
                     System.out.print("X");
-                } else if (tempRow == cell) {
+                } else if (row == cell) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");
