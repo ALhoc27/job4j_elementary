e066461ff8d1060d5c9e4f2aea4e2596e80e0576
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-03 21:15:17 +0500

    .

--- src/main/java/ru/job4j/array/MinDiapason.java
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
-        int min = array[start];
+        int min = array[start + 1];
         for (int i = start; i <= finish; i++) {
             if (min > array[i]) {
                 min = array[i];
