2accf34e1139b412ce2d18be1bb9c0ef6db78d3e
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-04 06:15:42 +0500

    .

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -2,15 +2,13 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int month = 1;
-        int ratioIvan = 3;
-        int ratioNikolay = 2;
-        if (ivan > nik) {
-           return --month;
-        }
-        while ((ivan * ratioIvan * month) < (nik * ratioNikolay)) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
             month++;
         }
         return month;
     }
 }
+
