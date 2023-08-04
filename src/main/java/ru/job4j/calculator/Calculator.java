8eae499507d41f88f9ab66e335457ecd3dec52a4
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-04 09:09:15 +0500

    .

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -14,12 +14,10 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator.plus(1, 2);
+        Calculator.plus(100, 500);
         Calculator.plus(10, 11);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
 
-        plus(1, 2);
-        plus(10, 11);
-
-        minus(1, 2);
-        minus(10, 11);
     }
 }
