d1ddcca934f5d11b93453249b08cbea5024f2d76
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-04 06:57:06 +0500

    .

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -11,35 +11,8 @@ public class ChessBoard {
         if (isValid(x1) && isValid(y1) && isValid(y2) && isValid(x2)) {
             if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = Math.abs(x2 - x1);
-                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
             }
         }
         return rsl;
-    }
-
-    public static int wayElephant(int x1, int x2) {
-        return Math.abs(x2 - x1);
-    }
-
-    public static int figureRook(int x1, int y1, int x2, int y2) {
-        int[] srgsWay = {x1, y1, x2, y2}; int rsl = 0;
-        boolean nextOper = true;
-        for (int i:srgsWay) {
-            if (i >= 0 && i <= 7) {
-                System.out.print("");
-            } else {
-                nextOper = false;
-            }
-        }
-
-        if ((nextOper) && (x1 == x2 || y1 == y2)) {
-            rsl = Math.abs(x2 - x1);
-            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
-        }
-        return rsl;
-    }
-
-    public static int wayRook(int x1, int x2, int y1, int y2) {
-        return Math.abs(x2 - x1) + Math.abs(y2 - y1);
     }
 }
