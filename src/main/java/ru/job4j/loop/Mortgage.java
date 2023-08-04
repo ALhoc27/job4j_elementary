deb5ff8fea1465500f12eaf9105086fd7db1c799
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-04 06:07:00 +0500

    .

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        var tempAmount = amount;
         percent = percent / 100;
 
-        while (tempAmount > 0) {
-            tempAmount = (tempAmount + (tempAmount * percent)) - salary;
+        while (amount > 0) {
+            amount = (amount + (amount * percent)) - salary;
             year++;
         }
         return year;
