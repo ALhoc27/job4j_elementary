1141a78d6b52f581ca2be5611ec56632f3ead219
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-04 08:37:41 +0500

    .

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,27 +2,11 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        if (rsl < 100) {
-            double rezrsl = 100 - rsl;
-            System.out.printf("До идеального веса, вам не хватает: %,.2f \n", rezrsl);
-            return rsl;
-        } else {
-            System.out.println("У вас идеальный вес! ");
-            return rsl;
-        }
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        if (rsl < 100) {
-            double rezrsl = 100 - rsl;
-            System.out.printf("До идеального веса, вам не хватает: %,.2f \n", rezrsl);
-            return rsl;
-        } else {
-            System.out.println("У вас идеальный вес!");
-            return rsl;
-        }
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
