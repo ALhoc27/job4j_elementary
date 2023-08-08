572e2f5c7af56126a414f674e0417a89cb6e5d74
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-08 06:54:31 +0500

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 # [#505049]]

---
+++ src/main/java/ru/job4j/mytest/Example.java
@@ -0,0 +1,37 @@
+package ru.job4j.mytest;
+
+public class Example {
+    int i, j;
+
+    Example(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    void show() {
+        System.out.println("i & j: " + i);
+    }
+}
+
+class B extends Example {
+    int k;
+
+    B(int i, int j, int k) {
+        super(i, j);
+        this.k = k;
+    }
+
+    void show(String msg) {
+        //super.show();
+        System.out.println("k: " + k);
+        System.out.println(msg);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        B subOb = new B(1, 2, 4);
+        subOb.show("66666");
+        subOb.show();
+    }
+}
