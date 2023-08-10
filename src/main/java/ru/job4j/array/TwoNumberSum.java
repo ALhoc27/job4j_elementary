fc70f5a52884cbf6bbcd1280d44f48072f28b1c1
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-10 14:40:06 +0500

    .

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -5,19 +5,16 @@ public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int[] temtArray = new int[]{};
         int i = 0;
-        int j = i + 1;
-        while (i <= array.length - 2) {
-            if (j != array.length) {
-                if (array[i] + array[j] == target) {
-                    temtArray = new int[]{i, j};
-                    break;
-                }
-                j++;
+        int j = array.length - 1;
+        while (j > i) {
+            if (array[i] + array[j] == target) {
+                return new int[] {i, j};
+            } else if (array[i] > target - array[j]) {
+                j--;
             } else {
-                j = i + 1;
                 i++;
             }
         }
-        return temtArray;
+        return new int[0];
     }
 }
