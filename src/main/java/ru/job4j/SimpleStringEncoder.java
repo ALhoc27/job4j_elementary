7ebec14ceaaeea492f2b1326b6280f9112b75f68
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-19 19:33:45 +0500

    .

--- src/main/java/ru/job4j/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/SimpleStringEncoder.java
@@ -3,18 +3,18 @@ package ru.job4j;
 public class SimpleStringEncoder {
 
     public static String encode(String input) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         char symbol = input.charAt(0);
         int counter = 1;
         for (int i = 1; i < input.length(); i++) {
             if (symbol == input.charAt(i)) {
                 counter++;
             } else {
-                result = (counter == 1 ? result + symbol : result + symbol + counter);
+                result = new StringBuilder((counter == 1 ? result.toString() + symbol : result.toString() + symbol + counter));
                 symbol = input.charAt(i);
                 counter = 1;
             }
         }
-        return (counter == 1 ? result + symbol : result + symbol + counter);
+        return (counter == 1 ? result.toString() + symbol : result.toString() + symbol + counter);
     }
 }
