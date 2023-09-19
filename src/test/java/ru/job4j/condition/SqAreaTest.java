58a6ef6d00de8e0da3df33071a1a06fd2b3ca18e
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-19 08:42:35 +0500

    .

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -8,46 +8,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class SqAreaTest {
 
     @Test
-    void whenP6K2Sdot2quare2() {
+    void whenP6K2exp2() {
         int expected = 2;
         int p = 6;
         double k = 2;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
-        System.out.println("whenP6K2Sdot2quareDot2 " + expected + " / " + out);
     }
 
     @Test
-    void whenP9K3Sdot2quare379() {
+    void whenP9K3exp3dot79() {
         float expected = 3.79f;
         int p = 9;
         double k = 3;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
-        System.out.println("whenP8K4Sdot2quareDot379 " + expected + " / " + out);
     }
 
     @Test
-    void whenP8K4Sdot2quare256() {
+    void whenP8K4exp2dot56() {
         float expected = 2.56f;
         int p = 8;
         double k = 4;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
-        System.out.println("whenP8K4Sdot2quareDot256 " + expected + " / " + out);
     }
 
     @Test
-    void whenP6K2Square2() {
-        int expected = 2;
-        int p = 6;
-        int k = 2;
-        double out = SqArea.square(p, k);
-        assertThat(out).isCloseTo(expected, offset(0.01));
-    }
-
-    @Test
-    void whenP4K1Square1() {
+    void whenP4K1exp1() {
         int expected = 1;
         int p = 4;
         int k = 1;
