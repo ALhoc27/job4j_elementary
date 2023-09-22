636a5c9ebccfa5cb2c8f196e1c7dfd83231077c2
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-22 13:56:12 +0500

    .

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,16 +4,14 @@ public class EndsWith {
 
     public static boolean endsWith(char[] word, char[] post) {
         boolean tempBoolean = false;
+        int tempCount = 0;
         for (int i = 0; i <= post.length - 1; i++) {
             if (word[word.length - i - 1] == post[post.length - i - 1]) {
-                tempBoolean = true;
-            } else {
-                tempBoolean = false;
-                break;
+                tempCount++;
+                tempBoolean = (tempCount == post.length);
             }
 
         }
         return tempBoolean;
-
     }
 }
