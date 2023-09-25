1c2a1fe4947a6032987dfe5aa802e3d1837e63a6
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-25 13:17:19 +0500

    .

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        int tempMinIndex;
 
         for (int j = 0; j < data.length - 1; j++) {
-            tempMinIndex = FindLoop.indexInRange(data, MinDiapason.findMin(data, j, data.length - 1), j, data.length - 1);
-            data = SwitchArray.swap(data, tempMinIndex, j);
+            int tempEl = MinDiapason.findMin(data, j, data.length - 1);
+            int tempMinIndex = FindLoop.indexInRange(data, tempEl, j, data.length - 1);
+            SwitchArray.swap(data, tempMinIndex, j);
         }
         return data;
     }
